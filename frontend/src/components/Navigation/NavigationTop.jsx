a93a02d3f46362c552e7cdd34afd880815afc71f
sujitChougule <sujitchougle2003@gmail.com>
2024-02-08 03:21:49 +0530

    created react routes and changes in forlder structure

--- frontend/src/components/Navigation/NavigationTop.jsx
+++ frontend/src/components/Navigation/NavigationTop.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import RefreshButton from './Buttons/RefreshButton';
-import AvatarProfile from './Avatar';
-import Search from './Search';
-import NewItemButton from './Buttons/NewItemButton';
-
+import React from "react";
+import AvatarProfile from "./Avatar";
+import Search from "./Search";
+import NewItemButton from "./Buttons/NewItemButton";
 
 const NavigationTop = () => {
-    return (
-        <nav className=' h-12 flex justify-between px-10 bg-gray-100 align-middle m-1 rounded-md'>
-            {/* <LogoButton/> */}
-            <AvatarProfile />
-            <Search />
-            <NewItemButton/>
-            <AvatarProfile />
-        </nav>
-    )
-}
+  return (
+    <nav className=" h-12 flex justify-between px-10 bg-gray-100 align-middle m-1 rounded-md">
+      {/* <LogoButton /> */}
+      <AvatarProfile />
+      <Search />
+      <NewItemButton />
+      <AvatarProfile />
+    </nav>
+  );
+};
 
-export default NavigationTop
+export default NavigationTop;
